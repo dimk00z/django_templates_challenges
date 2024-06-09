07814c4973728e1f468eddee6dab7767fd485ff0
Dmitriy <dimk00z@gmail.com>
2024-06-09 10:24:16 +0300

    Add level2

--- django_templates_homework/views/level_2/c_transactions_list.py
+++ django_templates_homework/views/level_2/c_transactions_list.py
@@ -3,12 +3,17 @@
 
 Обратите внимание, что потенциально мошеннические транзакции подсвечиваются красным цветом в таблице.
 """
+
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 from django_templates_homework.generators.transactions import generate_fake_transactions
 
 
 def transactions_list_view(request: HttpRequest) -> HttpResponse:
     transactions = generate_fake_transactions(num=100)
-    return render(request, 'level_2/transactions_list.html', context={"transactions": transactions})
+    return TemplateResponse(
+        request,
+        "level_2/transactions_list.html",
+        context={"transactions": transactions},
+    )
